8257e36f217aca3f2d3d728b6384aa46ac80394f
ShenHanWen <shenhanwen@wepie.com>
2025-01-07 19:35:42 +0800

    feat: 编辑器内GUI工具

---
+++ Editor/SettingsWindow.cs
@@ -0,0 +1,83 @@
+using UnityEditor;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+using System;
+
+namespace EmmyLuaSnippetGenerator
+{
+    [Serializable]
+    public struct SettingOptions
+    {
+        public string GeneratePath;
+        public string TargetNamespacesStr;
+        public bool AddCSPrefixToFields;
+
+        public static string SavePath => AppDomain.CurrentDomain.BaseDirectory + @"\EmmyLuaSnippetToolData\config.xml";
+
+        public override readonly string ToString()
+        {
+            return $"GeneratePath: {GeneratePath}, SupportNameSpacesStr: {TargetNamespacesStr}, AddCSPrefixToFields: {AddCSPrefixToFields}";
+        }
+    }
+
+    public sealed class SettingsWindow : EditorWindow
+    {
+        private SettingOptions _options;
+
+        [MenuItem("LuaType/设置")]
+        public static void ShowWindow()
+        {
+            GetWindow<SettingsWindow>("Lua类型注解文件设置");
+        }
+
+        private void OnEnable()
+        {
+            if (XmlHelper.TryLoadConfig(SettingOptions.SavePath, out SettingOptions settings))
+            {
+                _options = settings;
+            }
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("配置文件将被保存在源代码文件的相同目录中.");
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("生成类型注解文件的路\n(具体到文件名)");
+            _options.GeneratePath = EditorGUILayout.TextField(
+                _options.GeneratePath,
+                GUILayout.MinWidth(200)
+            );
+
+            GUILayout.Space(10);
+
+            GUILayout.Label("要生成注解的C#命名空间\n(多个命名空间用空格分隔)");
+            _options.TargetNamespacesStr = EditorGUILayout.TextField(
+                _options.TargetNamespacesStr,
+                GUILayout.MinWidth(200)
+            );
+
+            GUILayout.Space(10);
+
+            GUILayout.Label("为生成的类型字段添加CS.前缀");
+            _options.AddCSPrefixToFields = EditorGUILayout.Toggle(_options.AddCSPrefixToFields);
+            
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("保存配置文件"))
+            {
+                XmlHelper.SaveConfig(_options, SettingOptions.SavePath);
+                this.Close();
+            }
+
+            if (GUILayout.Button("取消"))
+            {
+                this.Close();
+            }
+        }
+    }
+}
+
+
---
+++ Util/XmlHelper.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace EmmyLuaSnippetGenerator
+{
+    public static class XmlHelper
+    {
+        public static bool SaveConfig<T>(T config, string filePath)
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            }
+
+            XmlSerializer serializer = new(typeof(T));
+
+            using StreamWriter writer = new(filePath);
+
+            serializer.Serialize(writer, config);
+
+            return true;
+        }
+
+        public static bool TryLoadConfig<T>(string filePath, out T config)
+        {
+            if (!File.Exists(filePath))
+            {
+                config = default;
+                return false;
+            }
+
+            XmlSerializer serializer = new(typeof(T));
+            using StreamReader reader = new(filePath);
+        
+            config = (T)serializer.Deserialize(reader);
+            return true;
+        }
+
+        public static void OpenWithDefaultEditor(string filePath)
+        {
+            Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
+        }
+    }
+}
+
